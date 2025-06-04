42b5c75d74087a1fc4dd0ae953b3f1d1bd22b3d0
AdonisBerishaS1X <rubingducks@gmail.com>
2025-06-04 19:09:37 +0200

    Moduli 26

---
+++ Moduli26/js/main.js
@@ -0,0 +1,38 @@
+/*console.log("test");*/
+
+//**document.write("Welcome To The Imperium");**//
+
+var x = 10;
+var _value = "Imperium"
+
+var a = 50+50;
+console.log(a);
+
+var b=20;
+var c=10;
+var d=b+c;
+
+console.log(d);
+
+var e=100;
+var f=50;
+var r=e-f;
+var sh=f*e;
+var p=e/f;
+
+console.log(r);
+console.log(sh);
+console.log(p);
+
+var n=99;
+n++;
+console.log(n);
+
+var y=101;
+y--;
+console.log(y);
+
+var x=15;
+x
+
+console.log(x);
