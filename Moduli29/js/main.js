0a25a2fefd38689db0381ffb510b4bf036f6b825
AdonisBerishaS1X <rubingducks@gmail.com>
2025-06-16 19:03:22 +0200

    moduli29

---
+++ Moduli29/js/main.js
@@ -0,0 +1,17 @@
+var programlang=['HMTL', 'CSS','JAVASCRIPT','PYTHON','C#']
+
+
+programlang.push('Python');
+console.log(programlang);
+
+programlang.pop();
+console.log(programlang):
+
+programlang.unshift('Test');
+console.log(programlang);
+
+programlang.shift();
+console.log(programlang);
+
+programlang.splice(0,4,'c++');
+console.log(programlang);
