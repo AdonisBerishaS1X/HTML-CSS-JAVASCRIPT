c10be8a775836cc75d0070af5bf5242dc5ffdb99
AdonisBerishaS1X <rubingducks@gmail.com>
2025-10-08 19:05:39 +0200

    mod36

--- moduli36/moduli36part2/js/main.js
+++ moduli36/moduli36part2/js/main.js
@@ -12,3 +12,20 @@ $('#btn').click(function(){
 $('#btn2').on('click',function(){
 	console.log('Second button is clicked');
 });
+
+$('square').clicl(function() {
+    $('#square').animate({
+      'width':'200px',
+	  'height':'200px'
+});
+});
+
+$('.testing').click(function() {
+	$('.testng').animate({
+		'left':"+=50px",
+		'opacity':0.25,
+		'fontSize':'30px'
+	},300,function(){
+      console.log("Animation is complete");
+	});
+});
